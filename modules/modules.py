2367c2e85301f7a1f844b0dae5d0d9cb5bc70df5
Putu Widyantara <putuwaw973@gmail.com>
2023-03-07 10:49:07 +0800

    Add function to generate random team and random picker

--- modules/modules.py
+++ modules/modules.py
@@ -1,4 +1,5 @@
 import time
+import random
 from tests import test_routes
 
 COMMANDS = {
@@ -6,12 +7,38 @@ COMMANDS = {
     'help': 'Gives information about all of the available commands',
     'ping': 'Measure the execution time to run test and send a message',
     'caps your sentence': 'Converts your sentence to uppercase',
+    'rand your sentence': 'Choose a random word from sentence, separated by space',
+    'team n_team your sentence': 'Generate a random team from sentence, separated by space'
 }
 
 
 def get_running_time(start_time):
     test_routes.test_index()
     return time.time() - start_time
+
+
+def get_random_team(n_team, member_list):
+    result = ''
+    if n_team > len(member_list):
+        result = 'Too many teams!'
+    elif n_team < 1:
+        result = 'Too few teams!'
+    else:
+        n_plus = len(member_list) % n_team
+        length = len(member_list)
+        result = ''
+        for i in range(n_team):
+            result += 'Team {}:\n'.format(i+1)
+            if i < n_plus:
+                result += member_list.pop(
+                    random.randint(0,
+                                   len(member_list) - 1)) + '\n'
+            for j in range(length // n_team):
+                result += member_list.pop(
+                    random.randint(0,
+                                   len(member_list) - 1)) + '\n'
+            result += '\n'
+    return result
 
 
 def hello():
